35e29a0e345c7bb4008838fd69b0d0fc86c004ba
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-06-24 14:46:46 +0300

    ex 2 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿class Program
+{
+    static async Task Main()
+    {
+        var check = new[] { "№1", "№2", "№3" };
+        var task = check.Select(Process).ToList();
+        await Task.WhenAll(task);
+    }
+
+    static async Task Process(string num_task)
+    {
+        PRINT($"Take order: #{num_task}");
+        await Task.Delay(1000);
+        PRINT($"Order #{num_task} processing...");
+        var rand = new Random();
+        int delay = rand.Next(2000, 5001); 
+        await Task.Delay(delay);
+        PRINT($"Ready: #{num_task}");
+    }
+    static void PRINT(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+    }
+}
