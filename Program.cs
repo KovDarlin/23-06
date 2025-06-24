c044cd13f605a8a68d450880e59804a08f446d82
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-06-24 14:21:11 +0300

    ex 1 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿class Program
+{
+    static async Task Main()
+    {
+        var paths = new[] { "file1.txt", "file2.txt" };
+        var readTask = paths.Select(async path =>
+        {
+            try
+            {
+                string text = await File.ReadAllTextAsync(path);
+                int count = Counting(text);
+                Console.WriteLine($"[{path}] - {count} words");
+                return count;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{path} - {ex.Message}");
+                return 0;
+            }
+        }).ToList();
+
+        int[] res = await Task.WhenAll(readTask);
+        int sum = res.Sum();
+        Console.WriteLine($"Total words - {sum}");
+    }
+
+    static int Counting(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text)) return 0;
+        var word = text.Split(new[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        return word.Length;
+    }
+}
